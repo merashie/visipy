68acca2fb080c81db7e1e16cc5d2ab4944c089bf
rootVIII <colleyloyejames@gmail.com>
2020-06-13 01:34:55 -0400

    VisiPy 0.1

--- visipy/visipy.go
+++ visipy/visipy.go
@@ -1,5 +1,6 @@
 /*
 
+
 BSD 3-Clause License
 Copyright (c) 2020, James Colley All rights reserved. Redistribution
 and use in source and binary forms, with or without modification, are
@@ -23,6 +24,7 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 */
 package main
